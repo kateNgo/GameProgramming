7f78fcec38ab051145ab87b09b64db239dd9bee8
phuongngovn <ngo.kate@yahoo.com>
2017-08-07 13:44:54 +1000

    activity week 2

---
+++ lab week 2/11885744-LabWeek2/Assets/PrintAndHide.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour {
+    public Renderer rend;
+    private int i;
+    private int random;
+    // Use this for initialization
+    void Start () {
+        i = 0;
+        random = Random.Range(200, 250);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        i++;
+        Debug.Log(rend.gameObject.name + ": " + i);
+        if ((rend.gameObject.CompareTag("Red")) && (i == 100))
+        {
+            rend.gameObject.SetActive(false);
+        }
+        else if ((rend.gameObject.CompareTag("Blue")) && (i == random))
+        {
+            rend.enabled = false;
+        }
+       
+    }
+}
